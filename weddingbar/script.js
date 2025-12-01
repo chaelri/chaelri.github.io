d9b8cb10c8d434345890a9558abdd6bbda9293d0
charliecayno <charliecayno@gmail.com>
2025-12-02 00:30:35 +0800

    Update script.js

--- weddingbar/script.js
+++ weddingbar/script.js
@@ -494,7 +494,8 @@ async function enableNotifications() {
   if (permission !== "granted") return;
 
   const token = await getToken(messaging, {
-    vapidKey: BOa8XHyFqlBP8Wn7BU4Z_Vut60wcGv4947ZwZeUN6TmPfhuHfnga1AaKG6jeZ2LjC8wUDnh9VcExWFNXaU3J0Y8,
+    vapidKey:
+      "BOa8XHyFqlBP8Wn7BU4Z_Vut60wcGv4947ZwZeUN6TmPfhuHfnga1AaKG6jeZ2LjC8wUDnh9VcExWFNXaU3J0Y8",
   });
 
   console.log("FCM Token:", token);
