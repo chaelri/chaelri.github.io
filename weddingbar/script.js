18c22fad4b60163346dc878d997c2c947dbcb604
Charlie Michael Cayno <charliecayno@gmail.com>
2025-12-05 23:57:51 +0800

    asd

--- weddingbar/script.js
+++ weddingbar/script.js
@@ -989,8 +989,27 @@ document.addEventListener("touchend", (e) => {
     tableViewPanel.classList.add("open");
 
     // push whole app left visually
-    document.querySelector("main").style.transform = "translateX(-30%)";
+    document.querySelector("main").style.transform = "translateX(-100%)";
     document.querySelector("main").style.transition = "0.35s ease";
+  }
+});
+
+document.addEventListener("touchend", (e) => {
+  if (!shouldOpenTable()) return;
+
+  touchEndX = e.changedTouches[0].screenX;
+  const diff = touchEndX - touchStartX;
+
+  // RIGHT SWIPE → OPEN
+  if (diff > 70) {
+    tableViewPanel.classList.add("open");
+    document.querySelector("main").style.transform = "translateX(-100%)";
+  }
+
+  // LEFT SWIPE → CLOSE
+  if (diff < -70 && tableViewPanel.classList.contains("open")) {
+    tableViewPanel.classList.remove("open");
+    document.querySelector("main").style.transform = "translateX(0)";
   }
 });
 
