02b81dbcddc33f1e7e7300f359551d45f009b05e
Charlie Michael Cayno <charliecayno@gmail.com>
2025-12-20 02:24:20 +0800

    Update script.js

--- devo/script.js
+++ devo/script.js
@@ -160,7 +160,7 @@ async function loadPassage() {
   const API_KEY = "AIzaSyAZsOkUSvWUCB14gXJQyNrCzCJtgW_JH7c"; // TEMP ONLY
   let testText = `Send ${titleForGemini} NASB2020 ver in this JSON list format [{book: "John", book_id: "JHN", chapter: 1, text: "In the beginning was the Word, and the Word was with God, and the Word was God.\n", verse: 1},{book: "John", book_id: "JHN", chapter: 1, text: "The same was in the beginning with God.\n", verse: 2}]. Send only the actual JSON [{}], no other words.`;
   const gemini = await fetch(
-    "https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash:generateContent?key=" +
+    "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=" +
       API_KEY,
     {
       method: "POST",
